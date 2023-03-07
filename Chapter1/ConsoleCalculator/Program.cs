3585e0a3708a28fdc204ecdae84cf8225cdedee5
zzyzhbs <onyyuan@icloud.com>
2023-03-08 00:58:16 +0800

    Upload homework1

---
+++ Chapter1/ConsoleCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input the two operands in different lines");
+            double A, B, C;
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please input the operator");
+            char op = char.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case '+': C = A + B; break;
+                case '-': C = A - B; break;
+                case '*': C = A * B; break;
+                case '/':
+                    if (Math.Abs(B) > 1e-15)
+                    {
+                        C = A / B;
+                    } 
+                    else
+                    {
+                        Console.WriteLine("B cannot be 0!\nPress to exit");
+                        Console.ReadLine();
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator!\nPress to exit");
+                    Console.ReadLine();
+                    return;
+            }
+            Console.WriteLine($"The result of {A} {op} {B} is {C}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter1/WinFormCalculator/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "-";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "+";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "*";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "/";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please input the two operands!", "Note", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                double x, y;
+                x = Convert.ToDouble(textBox1.Text);
+                y = (textBox2.Text == "") ? 0 : Convert.ToDouble(textBox2.Text);
+
+                switch (Convert.ToChar(label1.Text))
+                {
+                    case '+':      
+                        label3.Text = (x + y).ToString();
+                        break;
+                    case '-':
+                        label3.Text = (x - y).ToString();
+                        break;
+                    case '*':
+                        label3.Text = (x * y).ToString();
+                        break;
+                    case '/':
+                        if (y == 0)
+                        {
+                            label3.Text = "Infinite";
+                        }
+                        else
+                        {
+                            label3.Text = (x / y).ToString();
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("Please select the operator!", "Note", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label3.Text = "";
+        }
+    }
+}
